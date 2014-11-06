11079bc920df472f3cb88574cf61964053678787
vitaly <vit312@gmail.com>
2014-11-06 22:44:15 +0300

    add AltrExperimentManager

--- src/altr/Runner.java
+++ src/altr/Runner.java
@@ -8,13 +8,9 @@ package altr;
 import altr.entity.Person;
 import altr.experiment.Stage;
 import altr.experiment.Experiment;
-import altr.strategies.SimpleEgoisticStrategy;
 import altr.distributions.NormDistr;
-import altr.managers.GroupManager;
-import altr.managers.PersonManager;
+import altr.managers.AltrExperimentManager;
 import altr.managers.SimpleExperimentManager;
-import altr.strategies.SimpleGroupStrategy;
-import java.util.Collection;
 import javenue.csv.Csv;
 
 /**
@@ -24,24 +20,24 @@ import javenue.csv.Csv;
 public class Runner {  
     public static Csv.Writer writer = new Csv.Writer("result.csv").delimiter(';');
     
-    static int ITERATION_NUMBER = 100;
-    static int STEP_NUMBER = 20;
-    static int PEOPLE_NUM = 500;
+    static int ITERATION_NUMBER = 30000;
+    static int STEP_NUMBER = 10;
+    static int PEOPLE_NUM = 21;
     
     public static void main(String[] args) throws CloneNotSupportedException {         
 
         Person groupMan = new Person(10, true, 0, 1, 0, "name", null);
         Person egoist = new Person(10, true, 1, 0, 0, "name", null);
-
-        Experiment exp = new Experiment(1, ITERATION_NUMBER, "", "test1");        
-        Stage stage = new Stage(exp.getId(), STEP_NUMBER, new NormDistr(-.8, 30), 1);        
-        exp.addStage(stage);
-        System.out.println(exp); 
         
         for(int i = 1; i < PEOPLE_NUM; i++){
+            Experiment exp = new Experiment(1, ITERATION_NUMBER, "", "test1");
+            Stage stage = new Stage(exp.getId(), STEP_NUMBER, new NormDistr(-.1, 1), 1);
+            exp.addStage(stage);
+            System.out.println(exp);
             System.out.println("group size = " + i);
             Environment env = new Environment("test");
             SimpleExperimentManager eM = new SimpleExperimentManager(exp, env, groupMan, i, egoist, PEOPLE_NUM - i);
+            //AltrExperimentManager eM = new AltrExperimentManager(exp, env, groupMan, i, egoist, PEOPLE_NUM - i);
             eM.carryOut();
         }
         
---
+++ src/altr/managers/AltrExperimentManager.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package altr.managers;
+
+import altr.Environment;
+import static altr.Runner.writer;
+import altr.distributions.Distribution;
+import altr.entity.Group;
+import altr.entity.Offer;
+import altr.entity.Person;
+import altr.experiment.Experiment;
+import altr.strategies.SimpleAltruisticStrategy;
+import altr.strategies.SimpleEgoisticStrategy;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ *
+ * @author Vitaly
+ */
+public class AltrExperimentManager extends ExperimentManager {    
+       
+    Collection<Person> group;
+    Collection<Person> others;
+    double[] groupAvgMoney;
+    double[] othersAvgMoney;
+    double[] avgMoney;
+    double[] acceptanceCounters;
+    int altrSize;
+    SimpleAltruisticStrategy altrStrategy;
+
+    public AltrExperimentManager(Experiment experiment, Environment env, Person altr, int altrSize, Person egoist, int egoSize) throws CloneNotSupportedException {
+        super(experiment, env);
+        this.altrSize = altrSize;
+        groupAvgMoney = new double[stepNumber]; 
+        othersAvgMoney = new double[stepNumber]; 
+        avgMoney = new double[stepNumber]; 
+        acceptanceCounters = new double[stepNumber]; 
+        altrStrategy = new SimpleAltruisticStrategy(0.0);
+        altr.setStrategy(altrStrategy);
+        egoist.setStrategy(new SimpleEgoisticStrategy());
+        this.group = PersonManager.clonePerson(altr, altrSize);
+        this.others = PersonManager.clonePerson(egoist, egoSize);
+
+        Group g = new Group("test altr group", "group1");
+        gM.addGroup(g);
+        gM.addPeopleToGroup(group, g.getId());        
+        pM.getPeople().addAll(group);
+        pM.getPeople().addAll(others);
+    }
+    
+    @Override
+    protected void accept(Boolean isAccepted, Collection<Offer> offers) {
+        if (!isAccepted) return;
+        ArrayList<Person> p = (ArrayList)pM.getPeople();
+        ArrayList<Offer> o = (ArrayList)offers;
+        for(int i =0; i < pM.getPeople().size(); i++) {
+            Double money = p.get(i).getMoney();
+            p.get(i).setMoney(money + o.get(i).getAmmount());
+        }
+    }
+
+    @Override
+    protected void results() {      
+//        writer.comment("test");
+//        writer.value("Average capital").newLine();
+//        for(int i = 0; i < stepNumber; i++){
+//            writer.value(Double.toString(groupAvgMoney[i]/iterationNumber)).value(Double.toString(othersAvgMoney[i]/iterationNumber)).
+//                    value(Double.toString(avgMoney[i]/iterationNumber)).value(Double.toString(acceptanceCounters[i]/iterationNumber)).newLine();
+//        }
+        int last = stepNumber-1;
+        writer.value(Double.toString(groupAvgMoney[last]/iterationNumber)).value(Double.toString(othersAvgMoney[last]/iterationNumber)).
+            value(Double.toString(avgMoney[last]/iterationNumber)).value(Double.toString(acceptanceCounters[last]/iterationNumber)).
+            newLine();
+        System.out.println("Finished!");
+    }
+
+    @Override
+    protected void analysis() {
+        double avg = Analyzer.getAverageMoney(group);
+        double avg1 = Analyzer.getAverageMoney(others);
+        double avg2 = Analyzer.getAverageMoney(pM.getPeople());
+        groupAvgMoney[step] += avg;
+        othersAvgMoney[step] += avg1;
+        avgMoney[step] += avg2;  
+        acceptanceCounters[step] += acceptanceCounter;
+    }
+
+    @Override
+    protected Collection<Offer> generateOffers(Distribution dist, Collection<Person> people) {
+        Collection<Offer> offers = new ArrayList();
+        for(Person p: people) {
+            Offer offer = new Offer(dist.getValue(), p.getId());
+            offers.add(offer);
+            //System.out.println(offer);
+        }
+        return offers;
+    }
+
+    @Override
+    protected boolean isAccepted(Collection<Offer> offers) {
+        double votes = 0;
+        long number = pM.getPeople().size();
+        for(Person p: pM.getPeople()) {
+            if (p.getStrategy().vote(offers, pM.getPeople(),  p.getId())) votes++;
+        }
+        double percentage = votes / number;
+        return (percentage >= 0.5);
+    }
+
+    @Override
+    protected void postprocessing() {
+        altrStrategy.reset();
+        for(Person p: pM.getPeople()){
+            p.setActive(p.getMoney() > 0);
+        }
+    }
+    
+}
